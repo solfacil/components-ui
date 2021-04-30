1df7d6c8b147713da8d15c37b459e4eff99e8c65
Fernando Jesus <fernando.rodrigodejesus@gmail.com>
2021-04-30 09:42:18 -0300

    feat: start chart and config action yarn

---
+++ src/components/Charts/LineSeries/ChartLineSeries.stories.js
@@ -0,0 +1,28 @@
+import ChartLineSeries from './ChartChartLineSeries';
+
+export default {
+  title: 'Components/UI Elements/Charts/Line Series',
+  component: ChartLineSeries,
+  argTypes: {
+    demo: {
+      control: {
+        type: 'select',
+        options: ['1', '2', '3', '4'],
+      },
+      defaultValue: '1',
+    },
+  },
+
+  parameters: {
+    componentSubtitle: '',
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { ChartLineSeries },
+  template: '<ChartLineSeries v-bind="$props" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {};
---
+++ src/components/Charts/LineSeries/ChartLineSeries.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :id="id" class="chart-line-series">LineSeries</div>
+</template>
+
+<script>
+export default {
+  name: 'ChartLineSeries',
+
+  props: {
+    /** Specify a custom id */
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@scss/_chart-line-series';
+</style>
