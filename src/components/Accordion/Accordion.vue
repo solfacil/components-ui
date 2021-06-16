4bf2b11475296a5f150e6709ef0c51d90c8a5870
Antonio Souza <alfsouza708@gmail.com>
2021-06-16 15:38:39 -0300

    feat: accordion arrow animation

--- src/components/Accordion/Accordion.vue
+++ src/components/Accordion/Accordion.vue
@@ -1,5 +1,35 @@
 <template>
-  <div :id="id" class="accordion">Accordion</div>
+  <div :id="id" class="accordion">
+    <div class="description" @click="showContent = !showContent">
+      <h2>{{ title }}</h2>
+      <span
+        class="accordion__arrow"
+        :class="{ accordion__arrow_down: showContent }"
+      >
+        <svg
+          width="10"
+          height="12"
+          viewBox="0 0 10 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M5 1L5 11M5 11L9 7.36364M5 11L1 7.36364"
+            stroke="#666666"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </span>
+    </div>
+
+    <transition name="accordion-content">
+      <slot></slot>
+    </transition>
+
+    <hr v-show="bottomLine" />
+  </div>
 </template>
 
 <script>
@@ -12,10 +42,31 @@ export default {
       type: String,
       required: true,
     },
+
+    /** The text that appear before the button */
+    title: {
+      type: String,
+      required: true,
+      default: 'Title',
+    },
+
+    /** If the component instantiate open or closed */
+    open: {
+      type: Boolean,
+      default: false,
+    },
+
+    /** If the component have the bottom line separator */
+    bottomLine: {
+      type: Boolean,
+      default: false,
+    },
   },
 
   data() {
-    return {};
+    return {
+      showContent: this.open,
+    };
   },
 
   methods: {},
