3ec98622a445767290a073b449ecb54ee652d5e2
Antonio Souza <alfsouza708@gmail.com>
2021-06-29 20:06:48 -0300

    feat: storybook identation

--- src/components/Accordion/Accordion.stories.js
+++ src/components/Accordion/Accordion.stories.js
@@ -6,22 +6,21 @@ export default {
   argTypes: {},
 
   parameters: {
-    componentSubtitle: 'Accordion is a vertically collapsing element to show and hide content.',
+    componentSubtitle:
+      'Accordion is a vertically collapsing element to show and hide content.',
   },
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { Accordion },
-  template: '<Accordion v-bind="$props"><template #description-0>Example Description.</template><template #description-1><p>This</p><p>Is</p><p>Another</p><p>Example</p><p>Description.</p></template></Accordion>',
+  template:
+    '<Accordion v-bind="$props"><template #description-0>Example Description.</template><template #description-1><p>This</p><p>Is</p><p>Another</p><p>Example</p><p>Description.</p></template></Accordion>',
 });
 
 export const Default = Template.bind({});
 Default.args = {
   id: 'accordion-example',
   open: 0,
-  headers: [
-    'Example 1',
-    'Example 2',
-  ],
+  headers: ['Example 1', 'Example 2'],
 };
