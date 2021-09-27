52e0818723e11e4738423b9587975f7ac179afea
Ana Oliveira <ana.oliveira@solfacil.com.br>
2021-09-27 18:37:07 -0300

    feat: includ validations test

--- src/components/SelectInput/SelectInput.unit.spec.js
+++ src/components/SelectInput/SelectInput.unit.spec.js
@@ -25,6 +25,9 @@ beforeEach(() => {
       ],
       multiselect: false,
     },
+    data: () => ({
+      showOptions: false,
+    }),
   });
 });
 
@@ -39,4 +42,80 @@ describe('SelectInput - Unit', () => {
     expect(wrapper.props().id).toBe('select-input');
     expect(container.attributes().id).toBe('select-input');
   });
+
+  it('should icon close using prop', async () => {
+    const select = wrapper.find('[data-testid="select"]');
+    await wrapper.setData({ showOptions: true });
+    await select.trigger('click');
+
+    expect(wrapper.vm.showOptions).toBe(false);
+  });
+
+  it('should single selection of item', async () => {
+    await wrapper.setProps({
+      multiselect: false,
+    });
+    await wrapper.find('[data-testid="select"]').trigger('click');
+    await wrapper.findAll('li').at(0).trigger('click');
+
+    expect(wrapper.vm.showOptions).toBe(false);
+  });
+
+  it('should multiple selection of items', async () => {
+    await wrapper.setProps({
+      multiselect: true,
+    });
+    await wrapper.find('[data-testid="select"]').trigger('click');
+    await wrapper.findAll('li').at(0).trigger('click');
+    await wrapper.findAll('li').at(1).trigger('click');
+
+    expect(wrapper.emitted().input.length).toBe(2);
+    expect(wrapper.emitted().change.length).toBe(2);
+  });
+
+  it('should selection of item', async () => {
+    await wrapper.setProps({
+      multiselect: false,
+      options: [
+        {
+          value: '1',
+          name: 'Sou um cliente e quero tirar dúvidas',
+        },
+      ]
+    });
+    await wrapper.find('[data-testid="select"]').trigger('click');
+    await wrapper.findAll('li').at(0).trigger('click');
+    expect(wrapper.props().options).toStrictEqual([{
+      value: '1',
+      name: 'Sou um cliente e quero tirar dúvidas',
+    }]);
+  });
+
+  it('should selection of multiple item', async () => {
+    await wrapper.setProps({
+      multiselect: true,
+      options: [
+        {
+          value: '1',
+          name: 'Sou um cliente e quero tirar dúvidas',
+        },
+        {
+          value: '2',
+          name: 'Sou um integrador e quero tirar dúvidas',
+        },
+      ]
+    });
+
+    await wrapper.find('[data-testid="select"]').trigger('click');
+    await wrapper.findAll('li').at(0).trigger('click');
+    expect(wrapper.props().options).toStrictEqual([{
+      value: '1',
+      name: 'Sou um cliente e quero tirar dúvidas',
+    },
+    {
+      value: '2',
+      name: 'Sou um integrador e quero tirar dúvidas',
+    },]);
+  });
+
 });
