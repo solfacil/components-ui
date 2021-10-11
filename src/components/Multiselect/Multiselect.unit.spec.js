34edb10a5e0021f3c606a6797af88add64bd0466
Ana Oliveira <ana.oliveira@solfacil.com.br>
2021-10-11 18:41:21 -0300

    feat: initial test unit multiselect

--- src/components/Multiselect/Multiselect.unit.spec.js
+++ src/components/Multiselect/Multiselect.unit.spec.js
@@ -9,6 +9,16 @@ beforeEach(() => {
   wrapper = shallowMount(Multiselect, {
     propsData: {
       id: 'multiselect',
+      options: [
+        {
+          value: '1',
+          name: 'Sou um cliente e quero tirar dúvidas',
+        },
+        {
+          value: '2',
+          name: 'Sou um integrador e quero tirar dúvidas',
+        },
+      ],
     },
   });
 });
@@ -16,5 +26,55 @@ beforeEach(() => {
 describe('Multiselect - Unit', () => {
   it('should mount the component', () => {
     expect(wrapper.vm).toBeDefined();
+  });
+
+  it('should ID using prop', async () => {
+    const container = wrapper.find('div');
+
+    expect(wrapper.props().id).toBe('multiselect');
+    expect(container.attributes().id).toBe('multiselect');
+  });
+
+  it('should multiple selection of items', async () => {
+    await wrapper.setProps({
+      multiselect: true,
+    });
+    await wrapper.find('[data-testid="select"]').trigger('click');
+    await wrapper.findAll('li').at(0).trigger('click');
+    await wrapper.findAll('li').at(1).trigger('click');
+
+    expect(wrapper.emitted().input.length).toBe(2);
+    expect(wrapper.emitted().change.length).toBe(2);
+
+    await wrapper.findAll('li').at(1).trigger('click');
+
+    expect(wrapper.emitted().input.length).toBe(3);
+    expect(wrapper.emitted().change.length).toBe(3);
+  });
+
+  it('should remove special caracteres', async () => {
+    expect(wrapper.vm.removeSpecialCharacters('nômade')).toBe('nomade');
+  });
+
+  it('should return all options', async () => {
+    expect(wrapper.vm.searchItems('')).toStrictEqual([
+      {
+        value: '1',
+        name: 'Sou um cliente e quero tirar dúvidas',
+      },
+      {
+        value: '2',
+        name: 'Sou um integrador e quero tirar dúvidas',
+      },
+    ]);
+  });
+
+  it('should return filtered options', async () => {
+    expect(wrapper.vm.searchItems('cliente')).toStrictEqual([
+      {
+        value: '1',
+        name: 'Sou um cliente e quero tirar dúvidas',
+      },
+    ]);
   });
 });
