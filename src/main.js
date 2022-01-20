43409d873502008093bb2079012e9ca1bb4e1304
kevinbreaker <kevinbreaker2604@gmail.com>
2022-01-20 18:43:22 -0300

    refactor: :recycle: updating main.js to new vue 3 syntax

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import Vue from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 
 import '@scss/core/index.scss';
 
-Vue.config.productionTip = false;
+const app = createApp(App);
 
-new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+app.mount('#app');
