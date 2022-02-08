1e28aafb176f9ba42d52376466a902a0dca8a9bd
Breno Douglas <breninho94@gmail.com>
2022-02-08 12:05:09 -0300

    chore: add plugin with flag in babel env dev

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
   env: {
+    development: {
+      plugins: [
+        ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+      ],
+    },
     test: {
       plugins: ['require-context-hook'],
     },
